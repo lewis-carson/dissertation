0bda77d15938fb0ae30d3d2a506ad2d522918e40
Lewis <lewis.carson@durham.ac.uk>
2025-11-11 15:01:20 +0000

    remove Rust binpack loaders

--- binpack/src/reader/mod.rs
+++
@@ -1,6 +0,0 @@
-mod bitreader;
-mod compressed_reader;
-mod move_score_list_reader;
-
-pub use compressed_reader::CompressedReaderError;
-pub use compressed_reader::CompressedTrainingDataEntryReader;
--- binpack/src/writer/mod.rs
+++
@@ -1,8 +0,0 @@
-#![allow(dead_code)]
-
-mod bitwriter;
-mod compressed_writer;
-mod move_score_list;
-
-pub use compressed_writer::CompressedTrainingDataEntryWriter;
-pub use compressed_writer::CompressedWriterError;
